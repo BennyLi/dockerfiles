6236c4ee48c57880641188f476ad5fdd4c419b75
Benny Li <dev@benny-li.de>
2019-10-15 10:22:43 +0200

    Added Master PDF Editor 4 (Free Version without watermark)

---
+++ apps/masterpdfeditor/masterpdfeditor.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+container_name="masterpdfeditor"
+config_volume_name="softmaker-office"
+
+if [ -n "$(docker container ls -a -f name=$container_name -q)" ]
+then
+  docker stop "$container_name" > /dev/null
+  docker rm "$container_name" > /dev/null
+fi
+
+#####  ---------------  Volume Setup  ---------------  #####
+if [ -n "$(docker volume ls -f name=$config_volume_name -q)" ]
+then
+  docker volume create "$config_volume_name" > /dev/null
+fi
+
+
+#####  ---------------  Container Setup  ---------------  #####
+docker create --tty \
+              --name "$container_name" \
+              `# Open up the Display for the GUI` \
+              -e "DISPLAY=unix$DISPLAY" \
+              -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+              `# Mount the config and the documents folder` \
+              -v $config_volume_name:/home/dockuser/.config/:rw \
+              -v $HOME/Documents:/home/dockuser/Documents:rw \
+              bennyli/masterpdfeditor $@
+
+
+container_id=$(docker container ls -a -f name=$container_name -q)
+xhost +local:`docker inspect --format='{{ .Config.Hostname }}' $container_id`
+docker start $container_id
